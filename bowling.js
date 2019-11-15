cd3d1c39db9ffd7dc898103effa7998787cedb62
miriamcarretero <Miriam.Carretero@travelport.com>
2019-11-15 16:26:06 +0100

    commit 29

--- bowling.js
+++ bowling.js
@@ -11,22 +11,35 @@ export default class Game{
 		let score = 0;
 		let frameIndex = 0;
 		for (let frame = 0; frame < 10; frame++) {
-			if(rolls[frameIndex] == 10) {
-				// strike
-				score += 10 + 
-					rolls[frameIndex + 1] + 
-					rolls[frameIndex + 2];
+			if(isStrike(rolls, frameIndex)) {
+				score += 10 + strikeBonus(rolls, frameIndex);
 				frameIndex += 1;
 			} else if (isSpare(rolls, frameIndex)) {
-				score += 10 + rolls[frameIndex + 2];
+				score += 10 + spareBonus(rolls, frameIndex);
 				frameIndex += 2;
 			} else {
-				score += rolls[frameIndex] + rolls[frameIndex + 1];
+				score += sumOfBallsInFrame(rolls, frameIndex);
 				frameIndex += 2;
 			}
 		}
 		return score;
 	}
+}
+
+function isStrike(rolls, frameIndex) {
+	return rolls[frameIndex] === 10;
+}
+
+function strikeBonus(rolls, frameIndex) {
+	return rolls[frameIndex + 1] + rolls[frameIndex + 2]; 
+}
+
+function spareBonus(rolls, frameIndex) {
+	return rolls[frameIndex + 2];
+}
+
+function sumOfBallsInFrame(rolls, frameIndex) {
+	return rolls[frameIndex] + rolls[frameIndex + 1];
 }
 
 function isSpare(rolls, frameIndex) {
